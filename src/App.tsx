63d844028a6cd3dcc75f074b7f6c155af6811c53
77001 <alimzhanzhakashev@gmail.com>
2023-02-27 12:55:00 +0600

    Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import Header from '@/scenes/header';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SelectedPage } from './types/TSelectedPage';
 
 function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+    const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+    useEffect(() => {
+        const handler = () => {
+            if (window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage(SelectedPage.Home);
+            } else {
+                setIsTopOfPage(false);
+            }
+        };
+        window.addEventListener('scroll', handler);
+
+        return () => {
+            window.removeEventListener('scroll', handler);
+        };
+    }, []);
+
     return (
         <div className='app bg-gray-20'>
             <Header
+                isTopOfPage={isTopOfPage}
                 selectedPage={selectedPage}
                 setSelectedPage={(page) => setSelectedPage(page)}
             />
--- src/scenes/header/index.tsx
+++ src/scenes/header/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '@/assets/Logo.png';
 import Link from './Link';
 import { SelectedPage } from '@/types/TSelectedPage';
 import CusomButton from '@/UI/CusomButton';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 type Props = {
     selectedPage: SelectedPage;
+    isTopOfPage: boolean;
     setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Header = ({ selectedPage, setSelectedPage }: Props) => {
+const Header = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
     const flexBetween = 'flex itens-center justify-between';
+    const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+    const backGroundColor = isTopOfPage ? '' : 'bg-primary-100 drop-shadow';
+
     return (
         <nav>
-            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            <div className={`${backGroundColor}${flexBetween} fixed top-0 z-30 w-full py-6`}>
                 <div className={`${flexBetween} mx-auto w-5/6`}>
                     <div className={`${flexBetween} w-full gap-16`}>
                         {/* left side img */}
@@ -21,41 +28,83 @@ const Header = ({ selectedPage, setSelectedPage }: Props) => {
                             src={Logo}
                             alt='logo'
                         />
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexBetween} w-full`}>
+                                {/* links */}
+                                <div className={`${flexBetween} gap-8 text-sm`}>
+                                    <Link
+                                        page='Home'
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page='Benefits'
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page='Our Classes'
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page='Contact Us'
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                </div>
 
-                        {/* right side links */}
-                        <div className={`${flexBetween} w-full`}>
-                            {/* links */}
-                            <div className={`${flexBetween} gap-8 text-sm`}>
-                                <Link
-                                    page='Home'
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                                <Link
-                                    page='Benefits'
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                                <Link
-                                    page='Our Classes'
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
-                                <Link
-                                    page='Contact Us'
-                                    selectedPage={selectedPage}
-                                    setSelectedPage={setSelectedPage}
-                                />
+                                <div className={`${flexBetween} gap-8`}>
+                                    <p>Sing In</p>
+                                    <CusomButton setSelectedPage={setSelectedPage}>Become a member</CusomButton>
+                                </div>
                             </div>
-
-                            <div className={`${flexBetween} gap-8`}>
-                                <p>Sing In</p>
-                                <CusomButton setSelectedPage={setSelectedPage}>Become a member</CusomButton>
-                            </div>
-                        </div>
+                        ) : (
+                            <button
+                                onClick={() => setIsMenuToggled(true)}
+                                className='rounded-full bg-secondary-500 p-2'>
+                                <Bars3Icon className='h-6 w-6 text-white' />
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
+
+            {!isAboveMediumScreens && isMenuToggled && (
+                <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
+                    {/* close icon */}
+                    <div className='flex justify-end p-12'>
+                        <button onClick={() => setIsMenuToggled(false)}>
+                            <XMarkIcon className='h-6 w-6 text-gray-400' />
+                        </button>
+                    </div>
+
+                    {/* menu links */}
+
+                    <div className='ml-[33%] flex flex-col gap-10 text-2xl'>
+                        <Link
+                            page='Home'
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page='Benefits'
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page='Our Classes'
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page='Contact Us'
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
